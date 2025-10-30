e3895968c1e8646220afaf22767d14ecc8f467b0
Amara Navaneetha <amaranavaneetha@gmail.com>
2025-10-30 18:15:48 +0530

    HOme UI

--- RestApp/Models/Category.cs
+++ RestApp/Models/Category.cs
@@ -8,11 +8,11 @@ namespace restapp.Models
         [Key]
         public int CategoryId { get; set; }
 
-        [Required(ErrorMessage = "Empty category name is not allowed")]
+        
         [MaxLength(30, ErrorMessage = "Invalid size")]
         public string CategoryName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Empty category description is not allowed")]
+        
         [MaxLength(1500, ErrorMessage = "Invalid size")]
         public string CategoryDescription { get; set; } = string.Empty;
 
